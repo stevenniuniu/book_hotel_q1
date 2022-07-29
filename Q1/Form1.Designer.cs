5094b0dc028067d354eafeae769547c816b66b07
MULU <35390640+stevenniuniu@users.noreply.github.com>
2022-07-29 21:58:00 +0800

    20220726 backup

---
+++ Q1/Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace Q1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1A",
+            "1B",
+            "1C",
+            "1D",
+            "1E",
+            "2A",
+            "2B",
+            "2C",
+            "2D",
+            "2E",
+            "3A",
+            "3B",
+            "3C",
+            "3D",
+            "3E",
+            "4A",
+            "4B",
+            "4C",
+            "4D",
+            "4E"});
+            this.comboBox1.Location = new System.Drawing.Point(69, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Select Room";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(237, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Selected Room is:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(380, 57);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Check In";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(69, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Check Out";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(69, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Clean";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(69, 279);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(111, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Out of Service";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(69, 321);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(111, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Repaired";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 195);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(468, 21);
+            this.textBox1.TabIndex = 9;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(239, 151);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(111, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Show Available";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(362, 151);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(111, 23);
+            this.button7.TabIndex = 11;
+            this.button7.Text = "Show Occupied";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(479, 151);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 23);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "Show Vacant";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(596, 151);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 23);
+            this.button9.TabIndex = 13;
+            this.button9.Text = "Show Repair";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(770, 361);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Q1/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Q1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Text = "";
+        }
+
+        //Array for Room Status
+        //0 occupied, 1 vacant, 2 available, 3 repair
+        int[] room = {  2,2,2,2,2,//1A to 1E
+                            0,0,0,0,0,//2E to 2A
+                            1,0,3,0,1,//3A to 3E
+                            0,0,2,0,0 //4E to 4A
+                         };
+        string[] room_str = { "1A","1B","1C","1D","1E",
+                                "2E","2D","2C","2B","2A",
+                                "3A","3B","3C","3D","3E",
+                                "4E","4D","4C","4B","4A"
+        };
+
+        int selected_room = 0;
+        int num_room = 20;
+
+        //Check In
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (room[selected_room] == 2)//2 available
+            {
+                room[selected_room] = 0;//0 occupied
+            }
+            else
+            { 
+                //TODO:
+            }
+        }
+
+        //Check Out
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (room[selected_room] == 0)//0 occupied
+            {
+                room[selected_room] = 1;//1 vacant
+            }
+            else
+            {
+                //TODO:
+            }
+        }
+
+        //Clean
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (room[selected_room] == 1)//1 vacant
+            {
+                room[selected_room] = 2;//2 available
+            }
+            else
+            {
+                //TODO:
+            }
+        }
+
+        //Out of Service
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (room[selected_room] == 1)//1 vacant
+            {
+                room[selected_room] = 3;//3 repair
+            }
+            else
+            {
+                //TODO:
+            }
+        }
+
+        //Repair
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (room[selected_room] == 3)//3 repair
+            {
+                room[selected_room] = 1;//1 vacant
+            }
+            else
+            {
+                //TODO:
+            }
+
+            //test code
+            //string[] result = Array.ConvertAll(room, x => x.ToString());
+            //textBox1.Text = String.Concat(result);
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label3.Text = comboBox1.Text;
+
+            //Convert String to Index
+            switch (label3.Text) {
+                case "1A":
+                    selected_room = 0;
+                    break;
+                case "1B":
+                    selected_room = 1;
+                    break;
+                case "1C":
+                    selected_room = 2;
+                    break;
+                case "1D":
+                    selected_room = 3;
+                    break;
+                case "1E":
+                    selected_room = 4;
+                    break;
+                case "2E":
+                    selected_room = 5;
+                    break;
+                case "2D":
+                    selected_room = 6;
+                    break;
+                case "2C":
+                    selected_room = 7;
+                    break;
+                case "2B":
+                    selected_room = 8;
+                    break;
+                case "2A":
+                    selected_room = 9;
+                    break;
+                case "3A":
+                    selected_room = 10;
+                    break;
+                case "3B":
+                    selected_room = 11;
+                    break;
+                case "3C":
+                    selected_room = 12;
+                    break;
+                case "3D":
+                    selected_room = 13;
+                    break;
+                case "3E":
+                    selected_room = 14;
+                    break;
+                case "4A":
+                    selected_room = 15;
+                    break;
+                case "4B":
+                    selected_room = 16;
+                    break;
+                case "4C":
+                    selected_room = 17;
+                    break;
+                case "4D":
+                    selected_room = 18;
+                    break;
+                case "4E":
+                    selected_room = 19;
+                    break;
+
+                default:
+                    break;
+
+            }
+
+        }
+
+        //Show available
+        private void button6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            for (int i = 0; i < num_room; i++)
+            {
+                if (room[i] == 2)
+                {
+                    textBox1.Text += room_str[i] + " ";
+                }
+
+            }
+        }
+
+        //show occupied
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            for (int i = 0; i < num_room; i++)
+            {
+                if (room[i] == 0)
+                {
+                    textBox1.Text += room_str[i] + " ";
+                }
+
+            }
+        }
+
+        //show vacant
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            for (int i = 0; i < num_room; i++)
+            {
+                if (room[i] == 1)
+                {
+                    textBox1.Text += room_str[i] + " ";
+                }
+
+            }
+        }
+
+        //show repair
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+
+            for (int i = 0; i < num_room; i++)
+            {
+                if (room[i] == 3)
+                {
+                    textBox1.Text += room_str[i] + " ";
+                }
+
+            }
+        }
+    }
+}
